48ef16ee8d2b55b463a09d66be6181e764133095
qisedao0215@gmail.com <qisedao0215@gmail.com>
2014-05-05 19:43:23 +0800

    Initial Commit

---
+++ ZYAudioPlayer/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  ZYAudioPlayer
+//
+//  Created by qingyun on 14-5-5.
+//  Copyright (c) 2014年 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
